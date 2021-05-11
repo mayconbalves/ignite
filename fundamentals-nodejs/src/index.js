84b2d536760187f63f8cfe8568721e39a9758c5c
mayconbalves <mayconmustaine@hotmail.com>
2021-05-10 22:04:56 -0300

    feat: create simplify routers

---
+++ fundamentals-nodejs/src/index.js
@@ -0,0 +1,38 @@
+const express = require('express')
+
+const app = express()
+
+// receive json
+app.use(express.json())
+
+app.get('/courses', (req, res) => {
+  return res.json(
+    ["curso 1", "curso 2", "curso 3"]
+  )
+})
+
+app.post('/courses', (req, res) => {
+  return res.json(
+    ["curso 1", "curso 2", "curso 3", "curso 4"]
+  )
+})
+
+app.put('/courses/:id', (req, res) => {
+  return res.json(
+    ["curso 6", "curso 2", "curso 3", "curso 4"]
+  )
+})
+
+app.patch('/courses/:id', (req, res) => {
+  return res.json(
+    ["curso 6", "curso 7", "curso 3", "curso 4"]
+  )
+})
+
+app.delete('/courses/:id', (req, res) => {
+  return res.json(
+    ["curso 6", "curso 2", "curso 4"]
+  )
+})
+
+app.listen(3333)
